3411478f594d06eebcdbe6ec0ade16f45a8a4b2d
StilaYN <dist.avdeev2004@gmail.com>
2024-05-21 23:26:53 +0700

    Add Class with constants

---
+++ Properties/FileCore/Constants.cs
@@ -0,0 +1,18 @@
+﻿namespace SQLInterpreter.Properties.FileCore
+{
+    public static class Constants
+    {
+        public static char[] Types = new[] { 'C', 'D', 'N', 'L', 'M' };
+        public static byte Terminator = 0x0D;
+        public static byte NoMemo = 0x03;
+        public static byte Memo = 0x83;
+        public static bool IsCorrectType(char type)
+        {
+            foreach (var i in Types)
+            {
+                if (i == type) return true;
+            }
+            return false;
+        }
+    }
+}
