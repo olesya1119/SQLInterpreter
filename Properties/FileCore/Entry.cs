40eafaa7167a7d78193045ce0f0c019da914568d
StilaYN <dist.avdeev2004@gmail.com>
2024-05-25 22:50:00 +0700

    add commentaries

--- Properties/FileCore/Entry.cs
+++ Properties/FileCore/Entry.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 namespace SQLInterpreter.Properties.FileCore
 {
     /// <summary>
-    /// для работы с записью в файле
+    /// для работы с записью
     /// </summary>
     public class Entry
     {
@@ -22,12 +22,40 @@ namespace SQLInterpreter.Properties.FileCore
             while (true) yield return null;
             yield break;
         } 
-
+        /// <summary>
+        /// для перевода записи из одного формата в другой
+        /// </summary>
+        /// <param name="oldEntry">переводимая запись</param>
+        /// <param name="newEntryTemplate">новый формат записи, на основе заголовка</param>
+        /// <returns></returns>
         public static Entry ConvertEntry(Entry oldEntry, DbfHeader newEntryTemplate)
         {
             var generator = GetField(oldEntry.Header.Fields);
             generator.MoveNext();
             var entry = new Entry(newEntryTemplate);
+            entry.IsDeleted = oldEntry.IsDeleted;
+            foreach (var i in newEntryTemplate.Fields)
+            {
+                while (generator.Current != null && generator.Current.Name != i.Name) generator.MoveNext();
+                if (generator.Current != null)
+                {
+                    var f = generator.Current;
+                    var buf = new byte[i.Size]; 
+                    Buffer.BlockCopy(buf,0,entry.GetByte(),i.Offset,i.Size);
+                    byte size = (i.Size < f.Size) ? i.Size : f.Size;
+                    Buffer.BlockCopy(oldEntry.GetByte(),f.Offset,entry.GetByte(),i.Offset,size);
+                }
+            }
+            return entry;
+        }
+        
+        public static Entry UpdateEntry(Entry oldEntry, DbfHeader newEntryTemplate)//здесь ваша система,
+                                                                                   //учитывающая приведения типов
+        {
+            var generator = GetField(oldEntry.Header.Fields);
+            generator.MoveNext();
+            var entry = new Entry(newEntryTemplate);
+            entry.IsDeleted = oldEntry.IsDeleted;
             foreach (var i in newEntryTemplate.Fields)
             {
                 while (generator.Current != null && generator.Current.Name != i.Name) generator.MoveNext();
@@ -67,7 +95,12 @@ namespace SQLInterpreter.Properties.FileCore
             get => (_entry[0] == Constants.Delete);
             set => _entry[0] = (value) ? Constants.Delete : Constants.NoDelete;
         }
-        
+        /// <summary>
+        /// для получения значения записи по имени поля
+        /// </summary>
+        /// <param name="fieldName">имя получаемого поля</param>
+        /// <returns>возвращает данные в виде массива байтов</returns>
+        /// <exception cref="ArgumentException"></exception>
         public byte[] GetValue(string fieldName)
         {
             foreach (var i in _header.Fields)
@@ -83,7 +116,12 @@ namespace SQLInterpreter.Properties.FileCore
             }
             throw new ArgumentException("field with this name had not found");
         }
-
+        /// <summary>
+        /// для обновления значения в записи по имени поля
+        /// </summary>
+        /// <param name="fieldName">имя поля</param>
+        /// <param name="data">данные для записи</param>
+        /// <exception cref="ArgumentException"></exception>
         public void Update(string fieldName, byte[] data)
         {
             foreach (var i in _header.Fields)
@@ -101,7 +139,10 @@ namespace SQLInterpreter.Properties.FileCore
             }
             throw new ArgumentException("field with this name had not found");
         }
-
+        /// <summary>
+        /// для получения записи в виде массива байт
+        /// </summary>
+        /// <returns></returns>
         public byte[] GetByte()
         {
             return _entry;
