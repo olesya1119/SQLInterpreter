bd692261f3b28c629bd59816cb16ea0ca0c01421
Данил Кузнецов <danilkuzn2004@gmail.com>
2024-06-02 00:48:55 +0700

    Мелкие правки в memo файлах

--- FileCore/DbtBlock.cs
+++ FileCore/DbtBlock.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLInterpreter.Properties.FileCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,21 +9,20 @@ namespace SQLInterpreter.FileCore
 {
     internal class DbtBlock
     {
-        private static short blockSize = 512;
-        private byte[] _data = new byte[blockSize];
+        private byte[] _data = new byte[Constants.blockSize];
         public byte[] Data { get => _data; }
         public short Length
         { 
             get
             {
-                short len = (short)(blockSize - 1);
+                short len = (short)(Constants.blockSize - 1);
                 while (_data[len] == 0x0 && len != 0) len--;
                 return len;
             }
         }
         public DbtBlock(byte[] data)
         {
-            Buffer.BlockCopy(data, 0, _data, 0, data.Length-1);
+            Buffer.BlockCopy(data, 0, _data, 0, data.Length);
         }
     }
 }
